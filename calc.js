35acc65d7abaff345d5bf80bfd7d60633c0b691f
kurimaron <kurisyun4321@gmail.com>
2020-10-21 21:16:12 +0900

    ES記法でjest実行できない。

---
+++ calc.js
@@ -0,0 +1,7 @@
+export const sum = (x, y) => {
+  return x + y;
+};
+
+// export const sum;
+
+console.log(sum(1, 4));
---
+++ calc.test.js
@@ -0,0 +1,5 @@
+import { sum } from "./calc.js";
+
+test("Sum test", () => {
+  expect(sum(1, 3)).toBe(3);
+});
